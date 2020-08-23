3226ef8877551ddec09bb670ab3dff269e164867
Joe Quach <joe.t.quach@gmail.com>
2020-08-23 23:03:10 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,24 +6,11 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(user_input)
-   user_input.to_i - 1
+def input_to_index(input)
+  input.to_i - 1
 end
 
-def move(board, index, player)
-   board[index] = player
-end
 
-def position_taken?(board, index)
-   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-      return false 
-   else
-      return true
-   end
-end
-
-def valid_move?(board, index)
-   if index.between?(0,8) && !position_taken?(board, index)
-      return true
-   end
+def move(board, index, player = 'X')
+  board[index] = player
 end
